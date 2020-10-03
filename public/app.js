d0f1b95a76c910d52e7461433533d54d10e572ed
Alex <52643676+alexl0806@users.noreply.github.com>
2020-10-03 15:01:06 -0400

    firebase

--- public/app.js
+++ public/app.js
@@ -4,4 +4,8 @@ console.log(firebase)
 //sign up feature
 var email = document.getElementById("Email").value;
 var pass = document.getElementById("Password").value;
-var confirmPass = document.getElementById("Password").value;
+var confirmPass = document.getElementById("Password2").value;
+
+if (email.length < 4) {
+
+}
