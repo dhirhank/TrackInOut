3337e3e5f43b8c637dc34ef3e70a3f3c364c5ed4
Alex <52643676+alexl0806@users.noreply.github.com>
2020-10-03 14:50:34 -0400

    small things

--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
+//console log
 console.log(firebase)
+
+//sign up feature
+var email = document.getElementById("Email").value;
+var pass = document.getElementById("Password").value;
+var confirmPass = document.getElementById("Password").value;
