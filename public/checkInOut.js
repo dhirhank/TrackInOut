568b32d382a0dccaf71ca292825544dd589f5d60
Jaimin <jaiminjaffer123@gmail.com>
2020-10-03 22:10:59 -0400

    updated styling and backend storage

---
+++ public/checkInOut.js
@@ -0,0 +1,22 @@
+// const checkInBtn = document.getElementById('CheckIn');
+// const checkOutBtn = document.getElementById('CheckOut');
+
+
+
+// checkInBtn.onClick = () => {
+//    alert("Dhirhan");
+// }
+
+// checkOutBtn.onClick = () => {
+//    alert("Darun");
+// }
+
+if(document.getElementById('CheckIn').clicked == true)
+{
+   alert("button was clicked");
+}
+
+if(document.getElementById('CheckOut').clicked == true)
+{
+   alert("button was clicked");
+}
--- public/joinRoom.js
+++ public/joinRoom.js
@@ -1,9 +1,14 @@
+window.onload() = () => {
+    alert("")
+}
+
 console.log(firebase);
 
 const submitCode = document.getElementById('roomSubmit');
 var code = document.getElementById('inputRoomCode');
 
 submitCode.onlick = () => {
-
+    var check = db.collection('Room');
+    alert(check);
 
 }
